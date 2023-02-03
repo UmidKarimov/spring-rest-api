bdb0b48c31206c4d7976de452f41aed27910c139
Umid Karimov <karimovumid95@gmail.com>
2023-02-03 23:31:03 +0400

    add getemplBy id

--- src/main/java/spring/rest/controller/EmployeeController.java
+++ src/main/java/spring/rest/controller/EmployeeController.java
@@ -4,6 +4,7 @@ package spring.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.rest.entity.Employee;
@@ -21,6 +22,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> showAllEmployees(){
         return employeeService.getAllEmployee();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable("id") int id){
+        return employeeService.getEmployeeById(id);
     }
 
 }
